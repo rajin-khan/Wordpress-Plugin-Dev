79e82b5857f112d083b009c778768db062b46cf3
Rajin Khan <rajin.khan2001@gmail.com>
2025-09-28 14:26:21 +0600

    Initial commit

---
+++ skelementor-plugin/skelementor-plugin.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @package SkelementorPlugin
+ */
+
+/*
+Plugin Name: Skelementor Plugin
+Plugin URI: http://skelementor.com
+Description: First Custom Plugin for Skelementor.
+Version: 1.0.0
+Author: Rajin Khan
+Author URI: https://rajinkhan.com
+License: GPLv2 or later
+Text Domain: skelementor-plugin
+*/  
+
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+Copyright 2005-2015 Automattic, Inc.
+*/
+
+//if someone else tries to access, we won't let them. two ways to do it.
+
+// if ( ! defined('ABSPATH') ) {
+//     die;
+// }
+
+defined('ABSPATH') or die('You shall not pass!');
