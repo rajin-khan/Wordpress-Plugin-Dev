efd7183795ddac2771a7145281d6dc9d8a97a376
Rajin Khan <rajin.khan2001@gmail.com>
2025-09-28 15:38:15 +0600

    Learn about add action and creating a custom post type and safety after deactivation to retain CPT data

--- skelementor-plugin/skelementor-plugin.php
+++ skelementor-plugin/skelementor-plugin.php
@@ -31,52 +31,47 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 Copyright 2005-2015 Automattic, Inc.
 */
 
-//if someone else tries to access, we won't let them. three ways to do it.
-
-// if ( ! defined('ABSPATH') ) {
-//     die;
-// }
-
 defined('ABSPATH') or die('You shall not pass!');
-
-// if ( ! function_exists('add_action') ) { //could be any function that pre loads
-//     echo 'You shall not pass!';
-//     exit;
-// }
 
 
 class SkelementorPlugin {
   
-    //constructors in php oop are defined like this, no need to keep, but i am for reference.
-    function __construct() {}
+    function __construct() {
+        add_action('init', array($this, 'custom_post_type')); //so we search for the function right in this class, hence the $this variable.
+        //but calling the add_action is often unsafe and may fail (rarely ever does), so just in case, call it in the activate function directly too
+    }
 
-    //the three triggers defined as functions
+
     function activate() {
         // generate a CPT (custom post type)
+        //we cannot call the function directly (as this is oop) as it's not globally defined, so we call it like this:
+        $this -> custom_post_type();
+
         // flush rewrite rules
+        flush_rewrite_rules(); //we can simply callthis function because it's defined globally.
     }
     
     function deactivate() {
         // flush rewrite rules
+        flush_rewrite_rules();
     }
 
     function uninstall() {
         // delete CPT 
         // delete all the plugin data from the DB
     }
+
+    function custom_post_type() {
+        register_post_type( 'book', ['public' => true, 'label' => 'Books']); //this creates a custom post type (related to wordpress, figure out what this is later.)
+    } //to call this function on initialization we have to use wordpress's add_action hook, but since that's a part of procedural programming, not oop, we must call it in the constructor.
 }
 
-//vars in php are prefixed with $
 if ( class_exists('SkelementorPlugin')) {
-    $skelementorPlugin = new SkelementorPlugin(); //basic oop structure
+    $skelementorPlugin = new SkelementorPlugin();
 }
-
-//wordpress triggers stuff on 3 actions by default (hooks that are provided to plugins by default from wordpress),
 
 //on activation
-register_activation_hook(__FILE__, array($skelementorPlugin, 'activate')); //how php works, so, to call a function inside a class, we pass it as an array)
-//in the array, we first pass the class variable where it's initialized, and then the string of the function name that is to be triggered.
-//by writing __FILE__, we ask the hook to only look inside this file regardless of other paths to find the function defined.
+register_activation_hook(__FILE__, array($skelementorPlugin, 'activate'));
 
 //on deactivation
 register_deactivation_hook(__FILE__, array($skelementorPlugin, 'deactivate'));
