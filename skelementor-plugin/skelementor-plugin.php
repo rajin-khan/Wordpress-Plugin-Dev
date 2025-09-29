baf3b5c624689bf62b8cf58adafd04bed7641f77
Rajin Khan <rajin.khan2001@gmail.com>
2025-09-29 11:31:42 +0600

    Finish learning about separation of class concerns

--- skelementor-plugin/skelementor-plugin.php
+++ skelementor-plugin/skelementor-plugin.php
@@ -33,44 +33,43 @@ Copyright 2005-2015 Automattic, Inc.
 
 defined('ABSPATH') or die('You shall not pass!');
 
+if (!class_exists('SkelementorPlugin')) { //making sure class isn't duplicated
 
-class SkelementorPlugin {
+    class SkelementorPlugin {
 
-    function register() {
-        add_action('admin_enqueue_scripts', array($this, 'enqueue'));
+        function register() {
+            add_action('admin_enqueue_scripts', array($this, 'enqueue'));
+        }
+
+        protected function create_post_type() {
+            add_action('init', array($this, 'custom_post_type'));
+        }
+
+        function activate() {
+            require_once plugin_dir_path( __FILE__) . 'inc/skelementor-plugin-activate.php';
+            SkelementorPluginActivate::activate();
+        }
+
+        function custom_post_type() {
+            register_post_type( 'book', ['public' => true, 'label' => 'Books']);
+        }
+
+        function enqueue() {
+            wp_enqueue_style( 'mypluginstyle', plugins_url('/assets/mystyle.css', __FILE__));
+            wp_enqueue_script( 'mypluginscript', plugins_url('/assets/myscript.js', __FILE__));
+        }
     }
 
-    protected function create_post_type() {
-        add_action('init', array($this, 'custom_post_type'));
+    if ( class_exists('SkelementorPlugin')) {
+        $skelementorPlugin = new SkelementorPlugin();
+        $skelementorPlugin -> register();
     }
 
-    //we are no longer using the main file/function for activation/deactivation, we have delegated the task to separate files.
-    //because of that, we'll have to add some checks at the end (before register_activation/deactivation_hook)
+    //on activation
+    register_activation_hook(__FILE__, array($skelementorPlugin, 'activate'));
 
-    //we can alternatively also call the activation/deactivation hook like this:
-    function activate() {
-        require_once plugin_dir_path( __FILE__).'inc/skelementor-plugin-activate.php';
-        SkelementorPluginActivate::activate();
-    }
+    //on deactivation
+    require_once plugin_dir_path( __FILE__).'inc/skelementor-plugin-deactivate.php';
+    register_deactivation_hook(__FILE__, array('SkelementorPluginDeactivate', 'deactivate'));
 
-    function custom_post_type() {
-        register_post_type( 'book', ['public' => true, 'label' => 'Books']);
-    }
-
-    function enqueue() {
-        wp_enqueue_style( 'mypluginstyle', plugins_url('/assets/mystyle.css', __FILE__));
-        wp_enqueue_script( 'mypluginscript', plugins_url('/assets/myscript.js', __FILE__));
-    }
 }
-
-if ( class_exists('SkelementorPlugin')) {
-    $skelementorPlugin = new SkelementorPlugin();
-    $skelementorPlugin -> register();
-}
-
-//on activation
-register_activation_hook(__FILE__, array($skelementorPlugin, 'activate')); //alternative way to call static methods shown, now handled in the activate function in class.
-
-//on deactivation
-require_once plugin_dir_path( __FILE__).'inc/skelementor-plugin-deactivate.php'; //concatenating the directory of where the activation file is located relative to the current file with '.'
-register_deactivation_hook(__FILE__, array('SkelementorPluginDeactivate', 'deactivate')); //now we can call it without initializing the class, as the activate/deactivate methods are now static.
